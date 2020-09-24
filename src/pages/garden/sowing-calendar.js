88f232d1de78db2c67f5de0d1eea0939c93048fd
charlottefranklin <charlotte@cfranklin.com>
2020-09-24 13:37:30 +1200

    remove sowing img

--- src/pages/garden/sowing-calendar.js
+++ src/pages/garden/sowing-calendar.js
@@ -2,14 +2,12 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import {SeedsDisplay} from "./seeds-display";
-import seeds01 from "../../images/seeds01.gif"
-
 
 const PlantingCalendarPage = () => {
     return (
         <Layout>
             <h1>Sowing calendar</h1>
-            <img src={seeds01} alt="sowing table"/>
+            {/*<img src={seeds01} alt="sowing table"/>*/}
             <section className="flex-group">
             <SeedsDisplay month="august" />
             <SeedsDisplay month="september" />
