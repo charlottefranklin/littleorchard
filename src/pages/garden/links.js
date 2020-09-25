52046d2b3da278e29d95110c409a6c9beb83f50c
charlottefranklin <charlotte@cfranklin.com>
2020-09-25 16:34:29 +1200

    CF - update data

--- src/pages/garden/links.js
+++ src/pages/garden/links.js
@@ -8,7 +8,7 @@ import {shopsData} from "./links-data";
 const LinksPage = () => {
     return (
         <Layout>
-            <h1>Links</h1>
+            <h1>Directory</h1>
             <h2>Seeds and plants</h2>
             <section className={"flex-group link-group"}>
                 {shopsData.plantSeedSales.map((grower, i)=> <VegDisplay grower={grower} index={i}/>)}
