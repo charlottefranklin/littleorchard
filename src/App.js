0310f3e5fe1c1b84185bab18e9141914a66354e4
charlottefranklin <charlotte@cfranklin.com>
2020-10-01 15:08:46 +1300

    CF - line up the text.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
 function Main() {
     return <Layout>
-        <section><h2>A notebook for the New Zealand home gardener.</h2></section>
+        <section><h2>A New Zealand home gardener's notebook.</h2></section>
         <img src="https://live.staticflickr.com/5568/14973505329_41cc90fb19_k.jpg" alt="Our Garden"/>
     </Layout>
 }
