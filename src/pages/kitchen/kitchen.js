6de80cfd6350f70e7e48a7952b26d8bf7056fd3c
charlottefranklin <charlotte@cfranklin.com>
2021-03-08 02:14:57 +1300

    update many things

--- src/pages/kitchen/kitchen.js
+++ src/pages/kitchen/kitchen.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Layout from "../../components/layout";
 
+import roses from "../../images/kitchen/roses.jpg"
+import yoghurt from "../../images/kitchen/yoghurt.jpg"
+
+
 const KitchenPage = () => {
     return(
         <Layout>
             <h1>Kitchen</h1>
+            <img className="img-sml" src={roses} alt="roses"/><br/>
+            <img className="img-sml" src={yoghurt} alt="yoghurt"/>
         </Layout>
     );
 };
